2b19d38901c9efe00492c0a6d4daa856be204852
Gryffindor Common Room <115562641+its-garun@users.noreply.github.com>
2022-10-19 14:28:04 +0530

    Add files via upload

---
+++ NumConversion.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+using namespace std;
+int main()
+{	int ch;
+	cout<<"1.)To convert Decimal to Binary \n2.)To convert Binary to Decimal";
+	cin>>ch;
+	switch()
+	{
+	case 1: int n;
+			cout<<"Enter a number in decimal form: ";	
+			cin>>n;
+			cout<<dig;
+			}
+	     	break;
+	case 2: {
+			long x;
+			cin>>x;	
+			int rem,sum=0,i=0;
+			while(x!=0)
+			{
+		
+			rem=x%10;
+			cout<<"\nDecimal form of the given number is: "<<sum;
+	    	break;
+	        }
+	
+	}
+	getch();
+	return 0;			
+}
